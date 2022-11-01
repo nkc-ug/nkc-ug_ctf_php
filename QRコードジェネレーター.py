24702285211d3266832fb493b7a1a7106430f0d8
CCC621 <ec98721zzz@gmail.com>
2022-11-02 07:25:23 +0900

    QRコード生成アプリ完成

---
+++ QRコードジェネレーター.py
@@ -0,0 +1,33 @@
+#pip install qrcode[pil]
+#pip install mysqlclient
+
+import MySQLdb
+import qrcode
+import cv2
+
+#情報入力
+id = input("id>>")
+name = input("name>>")
+password = input("password>>")
+
+
+query = "INSERT INTO users VALUES ({0},{1},{2},false)".format(id,password,name)
+connection = MySQLdb.connect(
+    host='localhost',
+    port=4306,
+    user='root',
+    passwd='ropa20932060',
+    db='app',
+    charset='utf8')
+cursor = connection.cursor()
+cursor.execute(query)
+connection.commit()
+connection.close()
+
+
+#画像生成・保存・描写
+img = qrcode.make('http://172.18.99.1/QRlogin.php?id={0}&password={1}'.format(id,password))
+img.save("{0}_{1}.png".format(id,name))
+cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
+cv2.imshow("Image", cv2.imread("{0}_{1}.png".format(id,name), cv2.IMREAD_UNCHANGED))
+cv2.waitKey()
